496e60c1e5ef7e81636504223ec4cde77bade0ec
unknown <alejandro.hernandez4819@alumnos.udg.mx>
2025-02-19 22:41:24 -0600

    Add: Pretty print

--- Lexer.c
+++ Lexer.c
@@ -2,14 +2,15 @@
 #include <ctype.h>
 #include <string.h>
 
+
 // Definición de los tipos de tokens
 typedef enum {
-    TOKEN_KEYWORD,          // Palabra clave
-    TOKEN_IDENTIFIER,       // Identificador de variable
-    TOKEN_NUMBER,           // Número
-    TOKEN_OPERATOR,         // Operador  (+, -, *, /, =)
-    TOKEN_SYMBOL,           // Símbolo especial ({, }, (, ), ;)
-    TOKEN_UNKNOWN           // Carácter desconocido
+    TOKEN_KEYWORD,
+    TOKEN_IDENTIFIER,
+    TOKEN_NUMBER,
+    TOKEN_OPERATOR,
+    TOKEN_SYMBOL,
+    TOKEN_UNKNOWN
 } TokenType;
 
 // Función para verificar si una cadena es una palabra clave
@@ -26,6 +27,14 @@ int isKeyword(const char* str) {
 
 // Función para analizar el código y generar tokens
 void lexer(const char* code) {
+
+    char a = 160;       // á
+    char e = 130;       // é
+    char o = 162;       // ó
+    
+
+    printf("=========== An%clisis l%cxico del c%cdigo ===========\n\n", a, e, o);
+
     int i = 0;
     while (code[i] != '\0') {
         // Ignorar espacios en blanco y saltos de línea
@@ -44,9 +53,9 @@ void lexer(const char* code) {
             buffer[j] = '\0';
 
             if (isKeyword(buffer)) {
-                printf("TOKEN_KEYWORD: %s\n", buffer);
+                printf("[TOKEN_KEYWORD]     -> %s\n", buffer);
             } else {
-                printf("TOKEN_IDENTIFIER: %s\n", buffer);
+                printf("[TOKEN_IDENTIFIER]  -> %s\n", buffer);
             }
             continue;
         }
@@ -59,28 +68,30 @@ void lexer(const char* code) {
                 buffer[j++] = code[i++];
             }
             buffer[j] = '\0';
-            printf("TOKEN_NUMBER: %s\n", buffer);
+            printf("[TOKEN_NUMBER]      -> %s\n", buffer);
             continue;
         }
 
         // Identificar operadores
         if (code[i] == '+' || code[i] == '-' || code[i] == '*' || code[i] == '/' || code[i] == '=') {
-            printf("TOKEN_OPERATOR: %c\n", code[i]);
+            printf("[TOKEN_OPERATOR]    -> %c\n", code[i]);
             i++;
             continue;
         }
 
         // Identificar símbolos especiales
-        if (code[i] == '{' || code[i] == '}' || code[i] == '(' || code[i] == ')' || code[i] == ';') {
-            printf("TOKEN_SYMBOL: %c\n", code[i]);
+        if (code[i] == '{' || code[i] == '}' || code[i] == '(' || code[i] == ')' || code[i] == ';'|| code[i] == ',') {
+            printf("[TOKEN_SYMBOL]      -> %c\n", code[i]);
             i++;
             continue;
         }
 
         // Si no se reconoce el carácter, se marca como desconocido
-        printf("TOKEN_UNKNOWN: %c\n", code[i]);
+        printf("[TOKEN_UNKNOWN]     -> %c\n", code[i]);
         i++;
     }
+
+    printf("\n=================================================\n");
 }
 
 int main() {
